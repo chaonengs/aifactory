e3b1d0691fa9b6f243b1c144b58b7416e75d1f6b
loxxo <water2ice@gmail.com>
2023-06-02 23:34:06 +0800

    fix feishu events

--- src/pages/api/feishu/[...appId].ts
+++ src/pages/api/feishu/[...appId].ts
@@ -1,169 +1,156 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient, App, Prisma, AIResource } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient, App, Prisma, AIResource } from '@prisma/client';
 import * as lark from '@larksuiteoapi/node-sdk';
-import {chat, createConfiguration} from 'utils/server/openai';
+import { chat, createConfiguration } from 'utils/server/openai';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-const findApp = async (id:string) =>{return await prisma.app.findUnique({
+const findApp = async (id: string) => {
+  return await prisma.app.findUnique({
     where: {
-      id,
+      id
     },
     include: {
-        aiResource: true,
-      },
-  })
-}
-
-const getFeishuUser = async (client: lark.Client, userId: string) =>{
-    const req = {
-        user_id_type:'union_id',
-        department_id_type:''
+      aiResource: true
     }
-    const res = await client.contact.user.get({
-        params:{
-            user_id_type: 'union_id',
-            department_id_type:'department_id'
+  });
+};
+
+const getFeishuUser = async (client: lark.Client, userId: string) => {
+  const req = {
+    user_id_type: 'union_id',
+    department_id_type: ''
+  };
+  const res = await client.contact.user.get({
+    params: {
+      user_id_type: 'union_id',
+      department_id_type: 'department_id'
+    },
+    path: {
+      user_id: userId
+    }
+  });
+  return res.data?.user;
+};
+
+const createMessage = async (feishuClient: lark.Client, feishuData: {}, app: App & { aiResource: AIResource }) => {
+  const messageContent = feishuData.message.content;
+  const aiResult = await chat(createConfiguration(app), messageContent, app.aiResource.model as string);
+  const feishuSender = await getFeishuUser(feishuClient, aiResult.data.sender.sender_id);
+  await prisma.$transaction([
+    prisma.usage.create({
+      data: {
+        message: {
+          create: {
+            senderUnionId: feishuSender?.union_id as string,
+            sender: feishuSender?.name ? feishuSender.name : (feishuData.sender.sender_id as string),
+            content: feishuData.message.content as string,
+            answer: aiResult.data.choices[0].text,
+            appId: app.id
+          }
         },
-        path:{
-            user_id: userId
-        }
+        aiResourceId: app.aiResourceId,
+        promptTokens: aiResult.data.usage.prompt_tokens as number,
+        completionTokens: aiResult.data.usage.completion_tokens as number,
+        totalTokens: aiResult.data.usage.total_tokens as number
+      }
+    }),
+    prisma.aIResource.update({
+      where: { id: app.aiResourceId },
+      data: {
+        tokenRemains: app.aiResource.tokenRemains - aiResult.data.usage.total_tokens,
+        tokenUsed: app.aiResource.tokenUsed + aiResult.data.usage.total_tokens
+      }
+    }),
+    prisma.app.update({
+      where: { id: app.id },
+      data: {
+        tokenUsed: app.tokenUsed + aiResult.data.usage.total_tokens
+      }
+    })
+  ]);
+
+  return aiResult.data.choices[0].text;
+};
+
+const eventDispatcher = (app: App & { aiResource: AIResource }) => {
+  if (app.config === null) {
+    throw Error('App is not configed');
+  }
+
+  const config = app.config as Prisma.JsonObject;
+  const client = new lark.Client({
+    appId: config['appId'] as string,
+    appSecret: config['appSecret'] as string,
+    appType: lark.AppType.SelfBuild,
+    domain: config['domain'] as string
+  });
+  return new lark.EventDispatcher({
+    encryptKey: config['appEncryptKey'] as string,
+    verificationToken: config['appVerificationToken'] as string
+  }).register({
+    'im.message.receive_v1': async (data) => {
+      if (app.aiResource.tokenRemains <= 0) {
+        const chatId = data.message.chat_id;
+        const res = await client.im.message.create({
+          params: {
+            receive_id_type: 'chat_id'
+          },
+          data: {
+            receive_id: chatId,
+            content: JSON.stringify({ text: 'Token已耗尽，请联系相关人员添加Token' }),
+            msg_type: 'text'
+          }
+        });
+        return res;
+      } else {
+        const answer = await createMessage(client, data, app);
+        const chatId = data.message.chat_id;
+        const res = await client.im.message.create({
+          params: {
+            receive_id_type: 'chat_id'
+          },
+          data: {
+            receive_id: chatId,
+            content: JSON.stringify({ text: answer }),
+            msg_type: 'text'
+          }
+        });
+        return res;
+      }
     }
-    );
-    return res.data?.user
-}
-
-const createMessage = async (feishuClient: lark.Client,  feishuData:{}, app:App & {aiResource:AIResource}) => {
-    const messageContent = feishuData.message.content;
-    const aiResult = await chat(createConfiguration(app), messageContent, app.aiResource.model as string);
-    const feishuSender = await getFeishuUser(feishuClient, aiResult.data.sender.sender_id);
-    await prisma.$transaction(
-        [
-            prisma.usage.create(
-                {
-                    data:{
-                        message: {
-                            create: {
-                                senderUnionId: feishuSender?.union_id as string,
-                                sender: feishuSender?.name ? feishuSender.name: feishuData.sender.sender_id as string,
-                                content: feishuData.message.content as string,
-                                answer: aiResult.data.choices[0].text,
-                                appId: app.id,
-                            }
-                        },
-                        aiResourceId: app.aiResourceId,
-                        promptTokens: aiResult.data.usage.prompt_tokens as number,
-                        completionTokens: aiResult.data.usage.completion_tokens as number,
-                        totalTokens: aiResult.data.usage.total_tokens as number,
-                    }
-                }
-            ),
-            prisma.aIResource.update(
-                {
-                    where: {id: app.aiResourceId},
-                    data: {
-                        tokenRemains: app.aiResource.tokenRemains - aiResult.data.usage.total_tokens ,
-                        tokenUsed: app.aiResource.tokenUsed + aiResult.data.usage.total_tokens 
-                    }
-                }
-            ),
-            prisma.app.update(
-                {
-                    where: {id: app.id},
-                    data:{
-                        tokenUsed: app.tokenUsed + aiResult.data.usage.total_tokens 
-                    }
-                }
-            )
-        ],
-    )
-
-    return aiResult.data.choices[0].text;
-}
-
-
-const eventDispatcher = (app:App & {aiResource:AIResource}) => {
-    if (app.config === null) {
-        throw Error('App is not configed')
-    }
-
-
-    const config = app.config as Prisma.JsonObject;
-    const client = new lark. Client({
-        appId: config['appId'] as string,
-        appSecret: config['appSecret'] as string,
-        appType: lark.AppType.SelfBuild,
-        domain: config['domain'] as string,
-    });
-    return  new lark.EventDispatcher({
-        encryptKey: config['appEncryptKey'] as string,
-        verificationToken: config['appVerificationToken'] as string,
-    }).register({
-        'im.message.receive_v1': async (data) => {
-            if (app.aiResource.tokenRemains <= 0 ){
-                const chatId = data.message.chat_id;
-                const res = await client.im.message.create({
-                    params: {
-                        receive_id_type: 'chat_id',
-                    },
-                    data: {
-                        receive_id: chatId,
-                        content: JSON.stringify({text: "Token已耗尽，请联系相关人员添加Token"}),
-                        msg_type: 'text'
-                    },
-                });
-                return res;
-        
-            } else {
-                const answer = await createMessage(client,data,app);
-                const chatId = data.message.chat_id;
-                const res = await client.im.message.create({
-                    params: {
-                        receive_id_type: 'chat_id',
-                    },
-                    data: {
-                        receive_id: chatId,
-                        content: JSON.stringify({text: answer}),
-                        msg_type: 'text'
-                    },
-                });
-                return res;
-            }
-            
-        }
-    });
-}
+  });
+};
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { appId } = req.query;
-    let id = null;
-    if(Array.isArray(appId)){
-        id = appId[0];
-    } else {
-        id = appId;
-    }
-    if(id){
-        const app = await findApp(id);
-        if(app === null){
-            res.status(404).end('not found')
-        } else {
-            if(req.body && req.body['type'] &&  req.body['type'] === 'url_verification'){
-                res.end(JSON.stringify({challenge:req.body['challenge']}));
-            } else if (req.body && req.body['encrypt'] ){
-                console.log( req.body['encrypt']);
-                const r = lark.generateChallenge(req.body,{encryptKey: app.config['appEncryptKey']});
-                res.end(JSON.stringify(r.challenge));
-            }
-            else{
-                const dispatcher = eventDispatcher(app);
-                const data = req.body;
-                const result = await dispatcher.invoke(data);
-                res.end(result);
-            }
-
-        }
-
-    } else {
-        res.status(404).end('not found')
-    }
+  const { appId } = req.query;
+  let id = null;
+  if (Array.isArray(appId)) {
+    id = appId[0];
+  } else {
+    id = appId;
   }
+  if (id) {
+    const app = await findApp(id);
+    if (app === null) {
+      res.status(404).end('not found');
+    } else {
+      if (req.body && req.body['type'] && req.body['type'] === 'url_verification') {
+        res.end(JSON.stringify({ challenge: req.body['challenge'] }));
+      } else if (req.body && req.body['encrypt']) {
+        console.log(req.body['encrypt']);
+        const r = lark.generateChallenge(req.body, { encryptKey: app.config['appEncryptKey'] });
+        if (r.isChallenge) {
+          res.end(JSON.stringify(r.challenge));
+        } else {
+          const dispatcher = eventDispatcher(app);
+          const data = req.body;
+          const result = await dispatcher.invoke(data);
+          res.end(result);
+        }
+      }
+    }
+  } else {
+    res.status(404).end('not found');
+  }
+}
