052337792faf8b338c89b0a6f73c5b97954a9813
loxxo <water2ice@gmail.com>
2023-06-26 15:13:03 +0800

    微信 api地址env中读取，或用默认值

--- src/utils/server/const.ts
+++ src/utils/server/const.ts
@@ -20,5 +20,5 @@ export const OPENAI_ORGANIZATION =
 export const AZURE_DEPLOYMENT_ID =
   process.env.AZURE_DEPLOYMENT_ID || '';
 
-export const WEWORK_BASE_URL = 'https://qyapi.weixin.qq.com'
-export const WEWORK_PROXYED_BASE_URL = 'https://wework-naodong.chilunyc.com';
+export const WEWORK_BASE_URL = process.env.WEWORK_BASE_URL || 'https://qyapi.weixin.qq.com';
+export const WEWORK_PROXYED_BASE_URL = process.env.WEWORK_PROXYED_BASE_URL || 'https://wework-naodong.chilunyc.com';
